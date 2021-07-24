6a2521558b698abd41c1ef656fde8532a48705d4
everetttsang <everettt@usc.edu>
2021-07-24 16:31:49 -0700

    Fixed clearbutton bug

--- src/Button.js
+++ src/Button.js
@@ -1,11 +1,17 @@
 //import Checkbox from './components/Checkbox'
 const Button = () => {
+  var color = true;
+
   const click = (e) =>{
     var table = document.getElementById("taskTable")
     var row = table.insertRow()
 
     var cell1 = row.insertCell(0)
     var cell2 = row.insertCell(1)
+    cell1.style.backgroundColor = (color)? "AliceBlue" : "CornSilk"
+    cell2.style.backgroundColor = (color)? "AliceBlue" : "CornSilk"
+    color = !color
+
 
     cell1.innerHTML =  '<input type="checkbox" />'
     cell2.innerHTML = document.getElementById("textField").value
--- src/components/ClearButton.js
+++ src/components/ClearButton.js
@@ -1,12 +1,11 @@
 const ClearButton = () => {
 
   const click =(e) =>{
-    var rows = document.getElementById("taskTable").rows.length;
-    if (rows >1){
-      for (let i=1; i< rows; i++){
-        document.getElementById("taskTable").deleteRow(i);
-      }
-    }
+    var rows = document.getElementById("taskTable").rows.length
+    var firstRow = document.getElementById("taskTable").rows[0]
+    var tBody = document.getElementById("taskTable").tBodies[0].cloneNode(false)
+    tBody.appendChild(firstRow)
+    document.getElementById("taskTable").replaceChild(tBody, document.getElementById("taskTable").tBodies[0])
   }
   return (<center><button onClick={click} className='btn'> Clear </button></center>)
 }
